67843d701d8569bc4ee010cc4196a59058d94dba
NdegeFelix <findech6@gmail.com>
2023-12-02 22:42:17 +0300

    completed assignment

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env python3
 
+
+# In book.py
+
 class Book:
-    pass
-    
-        
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+        self.current_page = 1  # Default current page to 1
+
+    def flip_page(self):
+        if self.current_page < self.page_count:
+            self.current_page += 1
+            print(f"Flipped to page {self.current_page}")
+        else:
+            print("You've reached the end of the book.")
+
+    def display_info(self):
+        print(f"Title: {self.title}")
+        print(f"Total Pages: {self.page_count}")
+        print(f"Current Page: {self.current_page}")
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size, color, price):
+        self.brand = brand
+        self.size = size
+        self.color = color
+        self.price = price
+
+    def change_color(self, new_color):
+        self.color = new_color
+        print(f"The color of the {self.brand} shoe has been changed to {self.color}.")
+
+    def display_details(self):
+        print(f"Brand: {self.brand}")
+        print(f"Size: {self.size}")
+        print(f"Color: {self.color}")
+        print(f"Price: {self.price}")
